dfdd939952a2b1df390626b5cc4ec50f9b6d72cb
10012470 <10012470@e107m05.istic.univ-rennes1.fr>
2013-10-14 17:04:10 +0200

    +EntityMan

---
+++ tp3_taa/src/main/java/tp3/EntityMan.java
@@ -0,0 +1,37 @@
+package tp3;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class EntityMan {
+
+	private static volatile EntityManager instance = null;
+		
+	public EntityMan(){
+		super();
+	}
+	
+	public final static EntityManager getInstance() {
+        //Le "Double-Checked Singleton"/"Singleton doublement vérifié" permet 
+        //d'éviter un appel coûteux à synchronized, 
+        //une fois que l'instanciation est faite.
+        if (EntityMan.instance == null) {
+           // Le mot-clé synchronized sur ce bloc empêche toute instanciation
+           // multiple même par différents "threads".
+           // Il est TRES important.
+           synchronized(EntityMan.class) {
+             if (EntityMan.instance == null) {
+            	 
+            	// Use persistence.xml configuration
+     			EntityManagerFactory emf = Persistence.createEntityManagerFactory("example");
+     			// Retrieve an entity manager
+     			EntityManager em = emf.createEntityManager();
+            	 
+            	 EntityMan.instance = em;
+             }
+           }
+        }
+        return EntityMan.instance;
+    }
+}
